67ee50adff2fea2b63232c27ff11aac61d88f800
İsa Karaman <77787550+isakaraman@users.noreply.github.com>
2023-08-13 01:20:12 +0300

    first commit

---
+++ PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    float spawnTimer=0;
+    // Update is called once per frame
+    void Update()
+    {
+
+        // On spacebar press, send dog
+        spawnTimer += Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (spawnTimer >= 2)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                spawnTimer = 0;
+            }
+    
+        }
+
+    }
+}
